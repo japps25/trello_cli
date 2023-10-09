83fc70af9b9d3a16a62a83129df6264293b4f4ac
Jessica Apps <japps156@gmail.com>
2023-10-09 21:42:24 +0100

    Test updates - created a conftest file to initialise pytest - added tests for authentication and getting boards

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,17 +1,9 @@
 import pytest
 from trello_cli.trello_api import TrelloApi
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
-api_key = os.getenv("TRELLO_API_KEY")
-api_secret = os.getenv("TRELLO_API_SECRET")
-oauth_token = os.getenv("TRELLO_OAUTH_TOKEN")
-oauth_secret = os.getenv("TRELLO_OAUTH_KEY")
 
 
 @pytest.fixture(scope="session")
 def trello_api():
-    """Create DogAPI object"""
-    trello_api = TrelloApi(api_key, api_secret, oauth_token, oauth_secret)
-    return trello_api
+    """Create TrelloAPI object"""
+    return TrelloApi(api_key=os.getenv("TRELLO_API_KEY"),api_token=os.getenv("TRELLO_API_TOKEN"))
